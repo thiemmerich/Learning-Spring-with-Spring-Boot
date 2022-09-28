98ec41dbd914e5dd1f626335339fbf77ff201a53
Thiago Emmerich <thiago.emmerich@hotmail.com>
2022-09-28 20:01:14 -0300

    Finished the 3th chapter of the course about Service Tier

--- src/main/java/com/linkedin/util/AppStartupEvent.java
+++ src/main/java/com/linkedin/util/AppStartupEvent.java
@@ -1,42 +1,28 @@
 package com.linkedin.util;
 
-import com.linkedin.data.Guest;
-import com.linkedin.data.GuestRepository;
-import com.linkedin.data.Reservation;
-import com.linkedin.data.ReservationRepository;
-import com.linkedin.data.Room;
-import com.linkedin.data.RoomRepository;
+import java.util.Date;
+import java.util.List;
 
+import com.linkedin.business.ReservationService;
+import com.linkedin.business.RoomReservation;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+	private final ReservationService reservationService;
+	private final DateUtils dateUtils;
 
-	private final RoomRepository roomRepository;
-	private final GuestRepository guestRepository;
-	private final ReservationRepository reservationRepository;
-
-	public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository,
-			ReservationRepository reservationRepository) {
-		this.roomRepository = roomRepository;
-		this.guestRepository = guestRepository;
-		this.reservationRepository = reservationRepository;
+	public AppStartupEvent(ReservationService reservationService, DateUtils dateUtils) {
+		this.reservationService = reservationService;
+		this.dateUtils = dateUtils;
 	}
 
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
-		Iterable<Room> rooms = this.roomRepository.findAll();
-		rooms.forEach(System.out::println);
-		
-		Iterable<Guest> guests = this.guestRepository.findAll();
-		guests.forEach(System.out::println);
-		
-		Iterable<Reservation> reservations = this.reservationRepository.findAll();
+		Date date = this.dateUtils.createDateFromDateString("2022-01-01");
+		List<RoomReservation> reservations = this.reservationService.getRoomReservationsForDate(date);
 		reservations.forEach(System.out::println);
-		
-
 	}
-
 }
