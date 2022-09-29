23613caf465b50838a0ef73c22ed1ca66703c9a6
Thiago Emmerich <thiago.emmerich@hotmail.com>
2022-09-29 20:28:54 -0300

    Finished the 4th chapter about web pages with spring using thymeleaf and templates.

---
+++ src/main/java/com/linkedin/web/GuestController.java
@@ -0,0 +1,32 @@
+package com.linkedin.web;
+
+import java.util.Date;
+import java.util.List;
+
+import com.linkedin.data.Guest;
+import com.linkedin.data.GuestRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/guests")
+public class GuestController {
+
+	private final GuestRepository guestRepository;
+
+	public GuestController(GuestRepository guestRepository) {
+		this.guestRepository = guestRepository;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getReservations(Model model) {
+		List<Guest> guests = this.guestRepository.findAll();
+
+		model.addAttribute("guestList", guests);
+
+		return "hotel-guests";
+	}
+}
